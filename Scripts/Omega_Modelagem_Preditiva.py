f656377d9daba7baa21ded192d8cfac6a10e07ee
Pedro Henrique Velame <pedrovelame123@gmail.com>
2024-02-12 21:28:30 -0300

    att

--- Scripts/Omega_Modelagem_Preditiva.py
+++ Scripts/Omega_Modelagem_Preditiva.py
@@ -7,38 +7,35 @@ def load_data(file_path):
     # Carregar o DataFrame com a coluna alvo
     return pd.read_parquet(file_path)
 
-def train_and_save_models(df, target_column, models_to_select, save_path):
+def train_and_save_best_model(df, target_column, save_path):
     print("Inicializando ambiente PyCaret...")
     # Inicializar o ambiente PyCaret
     exp1 = setup(df, target=target_column, session_id=123)
 
-    print("Comparando modelos específicos para classificação binária...")
-    # Comparar modelos específicos para classificação binária
-    best_models = compare_models(include=models_to_select, sort='AUC')
+    print("Comparando modelos para classificação binária...")
+    # Comparar modelos para classificação binária
+    best_model = compare_models(sort='AUC', n_select=1)
 
-    print(f"Iterando sobre {len(best_models)} melhores modelos e treinando/salvando cada um...")
-    # Iterar sobre todos os melhores modelos e treinar/salvar cada um
-    for model in best_models:
-        print(f"Treinando o modelo {model}...")
-        try:
-            # Treinar o modelo
-            trained_model = create_model(model)
+    print(f"Treinando o melhor modelo: {best_model}...")
+    try:
+        # Treinar o melhor modelo
+        trained_model = create_model(best_model)
 
-            print(f"Avaliando o modelo {model}...")
-            # Avaliar o modelo
-            evaluate_model(trained_model)
+        print("Avaliando o melhor modelo...")
+        # Avaliar o modelo
+        evaluate_model(trained_model)
 
-            model_save_path = f"{save_path}/{model}"
-            print(f"Salvando o modelo {model} treinado em {model_save_path}...")
-            # Criar o diretório de salvamento se não existir
-            os.makedirs(model_save_path, exist_ok=True)
-            # Salvar o modelo treinado na pasta especificada
-            save_model(trained_model, model_name=model, model_only=True, verbose=False, system=True, 
-                       model_path=model_save_path)
-            print(f"Modelo {model} treinado e salvo com sucesso!")
+        model_save_path = f"{save_path}/{best_model}"
+        print(f"Salvando o melhor modelo treinado em {model_save_path}...")
+        # Criar o diretório de salvamento se não existir
+        os.makedirs(model_save_path, exist_ok=True)
+        # Salvar o modelo treinado na pasta especificada
+        save_model(trained_model, model_name=best_model, model_only=True, verbose=False, system=True, 
+                   model_path=model_save_path)
+        print(f"Melhor modelo treinado e salvo com sucesso!")
 
-        except Exception as e:
-            print(f"Erro ao treinar ou salvar o modelo {model}. Detalhes: {str(e)}")
+    except Exception as e:
+        print(f"Erro ao treinar ou salvar o melhor modelo. Detalhes: {str(e)}")
 
 if __name__ == "__main__":
     # Caminho para o arquivo parquet
@@ -47,20 +44,13 @@ if __name__ == "__main__":
     # Nome da coluna alvo
     target_column = "Target"
     
-    # Modelos específicos para classificação binária (adicione/remova conforme necessário)
-    specific_models = ['lr', 'dt', 'rf', 'et']
-    
-    # Caminho para a pasta onde os modelos serão salvos
+    # Caminho para a pasta onde o melhor modelo será salvo
     save_models_path = "C:/Users/Pedro/Documents/GitHub/Omega/Modelos_ML"
     
     print("Carregando os dados...")
     # Carregar os dados
     data = load_data(data_file_path)
     
-    print("Amostrando os dados...")
-    # Amostragem dos dados
-    data_sample = data.sample(frac=0.1, random_state=42)  # Usando 10% dos dados como amostra
-    
-    print("Treinando e salvando modelos...")
-    # Treinar e salvar modelos
-    train_and_save_models(data_sample, target_column, specific_models, save_models_path)
+    print("Treinando e salvando o melhor modelo...")
+    # Treinar e salvar o melhor modelo
+    train_and_save_best_model(data, target_column, save_models_path)
