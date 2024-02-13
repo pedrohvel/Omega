baefdec874c1b74518e2013d7d748e78a6862d78
Pedro Henrique Velame <pedrovelame123@gmail.com>
2024-02-13 12:56:41 -0300

    Adição do H2o automl

--- Scripts/Omega_Modelagem_Preditiva.py
+++ Scripts/Omega_Modelagem_Preditiva.py
@@ -1,41 +1,48 @@
-import os
+import h2o
+from h2o.automl import H2OAutoML
 import pandas as pd
-from pycaret.classification import *
 
 def load_data(file_path):
     print(f"Carregando dados do arquivo: {file_path}")
     # Carregar o DataFrame com a coluna alvo
     return pd.read_parquet(file_path)
 
+def preprocess_data(df):
+    # Adicione aqui seu pré-processamento, se necessário
+    return df
+
 def train_and_save_best_model(df, target_column, save_path):
-    print("Inicializando ambiente PyCaret...")
-    # Inicializar o ambiente PyCaret
-    exp1 = setup(df, target=target_column, session_id=123)
+    print("Amostrando os dados...")
+    # Amostragem dos dados
+    data_sample = df.sample(frac=0.1, random_state=42)  # Usando 10% dos dados como amostra
 
-    print("Comparando modelos para classificação binária...")
-    # Comparar modelos para classificação binária
-    best_model = compare_models(sort='AUC', n_select=1)
+    print("Dividindo e pré-processando os dados...")
+    # Divida e pré-processe os dados (se necessário)
+    h2o.init()
+    h2o_data = h2o.H2OFrame(data_sample)
+    
+    # Defina as features e a variável alvo
+    x = h2o_data.columns
+    y = target_column
+    x.remove(y)
 
-    print(f"Treinando o melhor modelo: {best_model}...")
-    try:
-        # Treinar o melhor modelo
-        trained_model = create_model(best_model)
+    # Inicialize o H2O AutoML
+    aml = H2OAutoML(max_runtime_secs=300, seed=42)
 
-        print("Avaliando o melhor modelo...")
-        # Avaliar o modelo
-        evaluate_model(trained_model)
+    # Treine o modelo
+    aml.train(x=x, y=y, training_frame=h2o_data)
 
-        model_save_path = f"{save_path}/{best_model}"
-        print(f"Salvando o melhor modelo treinado em {model_save_path}...")
-        # Criar o diretório de salvamento se não existir
-        os.makedirs(model_save_path, exist_ok=True)
-        # Salvar o modelo treinado na pasta especificada
-        save_model(trained_model, model_name=best_model, model_only=True, verbose=False, system=True, 
-                   model_path=model_save_path)
-        print(f"Melhor modelo treinado e salvo com sucesso!")
+    # Veja os resultados
+    lb = aml.leaderboard
+    print(lb)
 
-    except Exception as e:
-        print(f"Erro ao treinar ou salvar o melhor modelo. Detalhes: {str(e)}")
+    # Salve o modelo líder
+    leader = aml.leader
+    model_path = h2o.save_model(model=leader, path=save_path, force=True)
+    print(f"Melhor modelo salvo em: {model_path}")
+
+    # Encerre o cluster H2O
+    h2o.shutdown()
 
 if __name__ == "__main__":
     # Caminho para o arquivo parquet
